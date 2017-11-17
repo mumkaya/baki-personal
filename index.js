5d40821c1a0eb114fff73f92fb14e1da87bf9998
Benjamin Saphier <b.saphier@gmail.com>
2017-11-17 16:17:29 -0500

    git

---
+++ index.js
@@ -0,0 +1,19 @@
+const words = ['designer', 'creative director', 'technologist', 'gentleman'];
+
+$(document).ready(function() {
+    let text = $('.text-fade');
+    let textIndex = -1;
+    let wordIndex = -1;
+
+    function fadeText() {
+      ++textIndex;
+      ++wordIndex;
+      text.text(words[wordIndex % words.length]);
+      text.eq(textIndex % text.length)
+          .fadeTo(2000, 1)
+          .delay(2000)
+          .fadeTo(2000, 0, fadeText);
+    }
+
+    fadeText();
+});
